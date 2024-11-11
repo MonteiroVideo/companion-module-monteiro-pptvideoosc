68805fd29b5aa509676978c79d35c2d9924391a4
MonteiroVideo <166059439+MonteiroVideo@users.noreply.github.com>
2024-11-10 23:08:10 -0300

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,9 @@
+export function getVariableDefinitions() {
+    return [
+        { name: 'Timer', variableId: 'timer' },
+        { name: 'Timer Hours', variableId: 'timer_hours' },
+        { name: 'Timer Minutes', variableId: 'timer_minutes' },
+        { name: 'Timer Seconds', variableId: 'timer_seconds' },
+        { name: 'Slide Info', variableId: 'slide_info' }, // Nova variável adicionada
+    ];
+}
